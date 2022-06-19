75e02cdc41d0be5dee21a28287bc5fe14371f02e
dan.alboteanu <alboteanud@gmail.com>
2022-06-19 17:55:40 +0300

    First commit

---
+++ app/src/main/java/com/turceni/photographtools/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.turceni.photographtools.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
